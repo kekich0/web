d3da5223fc57dbbf9e4381f44775e7f45fc01150
kekich0 <153537509+kekich0@users.noreply.github.com>
2024-11-02 11:27:45 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function() {
+    //owlCarousel
+    $(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        margin: 5,
+        autoWidth: true
+    });
+    });
+
+    //Ініціалізація owlCarousel при відкритті випадаючого списку
+    $('.dropdown').on('shown.bs.dropdown', function () {
+    $('.owl-carousel').trigger('refresh.owl.carousel');
+    });
+    
+    //Календар
+    $("#myDiv").datepicker();
+});
+
+//Збільшення зображення
+function openImage(src) 
+{
+    const fullScreenContainer = document.getElementById('fullScreenContainer');
+    const fullScreenImage = document.getElementById('fullScreenImage');
+    fullScreenImage.src = src;
+    fullScreenContainer.style.display = 'flex';
+}
+
+//Закриття зображеня
+function closeImage() 
+{
+    const fullScreenContainer = document.getElementById('fullScreenContainer');
+    fullScreenContainer.style.display = 'none';
+}
+
